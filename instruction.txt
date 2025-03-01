6c2dfbcd0b290b98730619cc2cf2c4d7fafe48b4
Marvell Ayon <loewiayon12@gmail.com>
2025-03-01 13:55:24 +0800

    first commit

---
+++ instruction.txt
@@ -0,0 +1,1 @@
+lagay mo sa images yung mga rerename then run the exe file
---
+++ renamer.py
@@ -0,0 +1,29 @@
+import os
+import shutil
+
+imagesPath = "./images" 
+outputPath = "./output/" 
+
+if not os.path.exists(outputPath):
+    os.makedirs(outputPath)
+
+if not os.path.exists(imagesPath):
+    os.makedirs(imagesPath)
+
+files = os.listdir(imagesPath)
+# print (files)
+for i, file in enumerate(files):
+    filePath = os.path.join(imagesPath, file)
+    fileName = file.split('.')[0].lower()
+    extension = file.split('.')[1].lower()
+    # filename, extension = os.path.splitext(file)
+    extension = str(extension).lower()
+    if
+    if str(extension).lower() in ["jpg", "jpeg", "png"]:
+        # filename, extension = os.path.splitext(file)
+        newFilename = f"renamed{i + 1}.{extension}"
+        copiedFilePath = f"{outputPath}{fileName}"
+        renamed = f"{outputPath}{newFilename}"
+        shutil.copy(filePath, copiedFilePath)
+        os.replace(copiedFilePath, renamed)
+        print(f"Renamed {file} to {newFilename}")
